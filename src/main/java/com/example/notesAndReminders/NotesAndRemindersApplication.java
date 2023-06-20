c9900fa18f7d17e4ebdd2ddbc1afcbcda60ee441
PimonovNI <nikitapimonov6@gmail.com>
2023-06-20 19:07:08 +0300

    Empty project with properties

---
+++ src/main/java/com/example/notesAndReminders/NotesAndRemindersApplication.java
@@ -0,0 +1,13 @@
+package com.example.notesAndReminders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesAndRemindersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesAndRemindersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/notesAndReminders/NotesAndRemindersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notesAndReminders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesAndRemindersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
